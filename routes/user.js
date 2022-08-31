7bb58abd743428745dc5ac433a36a4e4b70bb3be
kawsar007 <imkawsar007@gmail.com>
2022-08-31 12:01:21 +0600

    Implement Get, Update & Change Password

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+var auth = require('../services/authentications');
+var checkRole = require('../services/checkRole');
+
 router.post('/signup', (req, res) => {
     let user = req.body;
     query = "select email,password,role,status from user where email=?"
@@ -71,12 +74,12 @@ router.post('/forgotPassword', (req, res) => {
                     from: process.env.EMAIL,
                     to: results[0].email,
                     subject: 'Password by Cafe Management System',
-                    html: '<p><b>Your Login details for Cafe Management System</b><br/><b>Email:</b>' + results[0].email + '<br/><b>Password: </b>'+results[0].password+'<br><a href="http://localhost:4200/">Click here to login</a></p>'
+                    html: '<p><b>Your Login details for Cafe Management System</b><br/><b>Email:</b>' + results[0].email + '<br/><b>Password: </b>' + results[0].password + '<br><a href="http://localhost:4200/">Click here to login</a></p>'
                 };
-                transporter.sendMail(mailOptions,function(error, info){
-                    if(error){
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
                         console.log(error);
-                    } else{
+                    } else {
                         console.log('Email sent: ' + info.response);
                     }
                 })
@@ -84,6 +87,63 @@ router.post('/forgotPassword', (req, res) => {
             }
         } else {
             return res.status(500).json(err)
+        }
+    })
+})
+
+router.get('/get', auth.authenticateToken, checkRole.checkRole, (req, res) => {
+    var query = "select id,name,email,contactNumber,status from user where role='user'";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.patch('/update', auth.authenticateToken, checkRole.checkRole, (req, res) => {
+    let user = req.body;
+    var query = "update user set status=? where id=?";
+    connection.query(query, [user.status, user.id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "User id does not exist" });
+            }
+            return res.status(200).json({ message: "User Updated Successfully." })
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+router.get('/checkToken', auth.authenticateToken, (req, res) => {
+    return res.status(200).json({ message: 'true' })
+})
+
+router.post('/changePassword', auth.authenticateToken, (req, res) => {
+    const user = req.body;
+    const email = res.locals.email;
+    var query = "select *from user where email=? and password=?";
+    connection.query(query, [email, user.oldPassword], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                return res.status(400).json({ message: "Incorrect Old Password." })
+            }
+            else if (results[0].password == user.oldPassword) {
+                query = "update user set password=? where email=?";
+                connection.query(query, [user.newPassword, email], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json({ message: "Password Update Successfully." })
+                    } else {
+                        return res.status(500).json(err)
+                    }
+                })
+            } else {
+                return res.status(400).json({ message: "Something went wrong. Please try again later." })
+            }
+        } else {
+            return res.status(500).json(err);
         }
     })
 })
--- table.sql
+++ table.sql
@@ -10,3 +10,9 @@ create table user(
 );
 
 insert into user(name,contactNumber,email,password,status,role)values('Admin','01736467005','admin@gmail.com','admin'.'true','admin');
+
+-- create table category(
+--     id int NOT NULL AUTO_INCREMENT,
+--     name varchar(255) NOT NULL,
+--     primary key(id)
+-- )

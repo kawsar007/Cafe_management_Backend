c8524e7649a4778fd5c7fff52f78844ed7e1fad6
kawsar007 <imkawsar007@gmail.com>
2022-08-31 09:07:23 +0600

    Implement forgot password functionalities

--- routes/user.js
+++ routes/user.js
@@ -3,24 +3,25 @@ const connection = require('../connection');
 const router = express.Router();
 
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 router.post('/signup', (req, res) => {
     let user = req.body;
     query = "select email,password,role,status from user where email=?"
-    connection.query(query, [user.email],(err, results) => {
-        if(!err) {
-            if(results.length <= 0) {
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
                 query = "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?, 'false','user')";
-                connection.query(query,[user.name, user.contactNumber,user.email,user.password], (err,results) => {
-                    if(!err){
-                        return res.status(200).json({message:"Successfully Registered."})
-                    } else{
+                connection.query(query, [user.name, user.contactNumber, user.email, user.password], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json({ message: "Successfully Registered." })
+                    } else {
                         return res.status(500).json(err);
                     }
                 })
-            }else {
-                return res.status(400).json({message: "Email Already Exist..!"});
+            } else {
+                return res.status(400).json({ message: "Email Already Exist..!" });
             }
         } else {
             return res.status(500).json(err);
@@ -28,21 +29,58 @@ router.post('/signup', (req, res) => {
     })
 })
 
-router.post('/login', (req,res) => {
+router.post('/login', (req, res) => {
     const user = req.body;
     query = "select email,password,role,status from user where email=?";
-    connection.query(query,[user.email],(err,results) => {
-        if(!err){
-            if(results.length <= 0 || results[0].password != user.password){
-                return res.status(401).json({message:"Incorrect Username or Password!"})
-            } else if(results[0].status === 'false'){
-                return res.status(401).json({message:"Wait for Admin Approval"})
-            } else if(results[0].password == user.password){
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: "Incorrect Username or Password!" })
+            } else if (results[0].status === 'false') {
+                return res.status(401).json({ message: "Wait for Admin Approval" })
+            } else if (results[0].password == user.password) {
                 const response = { email: results[0].email, role: results[0].role }
                 const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8hr' })
                 res.status(200).json({ token: accessToken });
-            } else{
-                return res.status(400).json({message: "Something went wrong.Please try again later."});
+            } else {
+                return res.status(400).json({ message: "Something went wrong.Please try again later." });
+            }
+        } else {
+            return res.status(500).json(err)
+        }
+    })
+})
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+})
+
+router.post('/forgotPassword', (req, res) => {
+    const user = req.body;
+    query = "select email,password from user where email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                return res.status(200).json({ message: "Password sent successfully to your email." })
+            } else {
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'Password by Cafe Management System',
+                    html: '<p><b>Your Login details for Cafe Management System</b><br/><b>Email:</b>' + results[0].email + '<br/><b>Password: </b>'+results[0].password+'<br><a href="http://localhost:4200/">Click here to login</a></p>'
+                };
+                transporter.sendMail(mailOptions,function(error, info){
+                    if(error){
+                        console.log(error);
+                    } else{
+                        console.log('Email sent: ' + info.response);
+                    }
+                })
+                return res.status(200).json({ message: "Password sent successfully to your email." })
             }
         } else {
             return res.status(500).json(err)
